d4734ab94c907082b20f41041f2939c8fd161b84
Jonathan Lee <jonjlee@gmail.com>
2024-11-22 17:41:42 -0800

    Clean up unused files

--- explore/bin/start.sh
+++
@@ -1,4 +0,0 @@
-# Start streamlit in virtual environment. 
-# To run interactively without script, be sure to first: pipenv shell
-# Disable CORS and Xsfr protection per https://github.com/orgs/community/discussions/18038
-pipenv run streamlit run app.py --server.enableCORS false --server.enableXsrfProtection false
